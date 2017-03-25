adcaed88c377b83260683fcf197cf66901ba67cd
Erick-Huang <erick.huang@advantech.com.tw>
2017-03-26 01:38:24 +0800

    Have 2 changes
    1. Add lib.a files
    2. Modify Makefile

--- main.cpp
+++ main.cpp
@@ -1,34 +1,20 @@
 #include "mbed.h"
-#include "main.h"
-#include "IoTest.h"
-#include "RfTest.h"
-#include "simpleCli.h"
+#include "sigfox_api.h"
 
-DigitalOut power(CB_PWR_ON);
 Serial pc(SERIAL_TX, SERIAL_RX);
 
 // main() runs in its own thread in the OS
 // (note the calls to Thread::wait below for delays)
 int main() {
-    power = 1;
-      
+    int i = 1;
+	sfx_u8 *ptr;
+	sfx_u8 size;    
+    
     pc.baud(115200);
-    pc.printf("Hello World !!!!\r\n");
+	SIGFOX_API_get_version( &ptr, &size);
     
-    RFInit();
-    
-    simple_cli_loop(&pc);
-    // Enter runTX, never coming back
+    while(1) { 
+        ;
+    }
 }
 
-
-int Main_Menu(int index, char *string, int len, void *printer) {
-	if(string != NULL && len != 0) return 0;
-	simple_cmds cmds[] = {
-			{ AP_NAME/*title*/,  		                    NULL,NULL },
-            { "IO Test",                                    0,IOTest_Menu},
-			{ "RF Test",				                    0,RFTest_Menu },
-			{ NULL,											NULL,NULL }
-	};
-	return simple_cli_list(cmds);
-}
